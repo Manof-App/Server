857f7e44db65285486049cad01c4427fb0f67d6b
oNionUxx <omerlubko@gmail.com>
2020-10-20 12:31:16 +0300

    Adding request timer for wake up server

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,5 +1,5 @@
-const sgMail = require('@sendgrid/mail');
-const chalk = require('chalk')
+const sgMail = require("@sendgrid/mail");
+const chalk = require("chalk");
 
 const sendGridApiKey = process.env.SENDGRID_API_KEY;
 
@@ -8,38 +8,35 @@ sgMail.setApiKey(sendGridApiKey);
 const sendWelcomeEmail = (email, password) => {
   sgMail.send({
     to: email,
-    from: 'manofApp2020@gmail.com',
+    from: "manofApp2020@gmail.com",
     subject: 'הצטרפותך למערכת מנו"ף',
-    html: `<div style="width: 100px; height:100px; border:">
-        <h1 style="color: #333">סיסמתך החדשה</h1>
-       <h3 style="font-size: 20
-       px; font-weight:bold;">ברוך הבא למערכת מנו"ף</h3>
-          <h4>פרטי ההתחברות שלך הם :</h4>
-          <p>שם משתמש : ${email} </p>
-          <p>סיסמא : ${password} </p> 
-        </div>
-        `,
+    html: `<div dir="rtl" style="width: 100px; height:100px; border:">
+            <h1 style="color: #333">סיסמתך החדשה</h1>
+            <h3 style="font-size: 20px; font-weight:bold;">ברוך הבא למערכת מנו"ף</h3>
+            <h4>פרטי ההתחברות שלך הם :</h4>
+            <p>שם משתמש : ${email} </p>
+            <p>סיסמא : ${password} </p> 
+          </div>`,
   });
 };
 
 const resetPassword = (email, password) => {
-  sgMail.send({
-    to: email,
-    from: 'manofApp2020@gmail.com',
-    subject: 'איפוס סיסמת משתמש',
-    html: `<div style="width: 100px; height:100px; border:">
-    <h1 style="color: #333">סיסמתך החדשה</h1>
-   <p style="font-size: 20
-   px; font-weight:bold;">${password}</p>
-
-    </div>
-    `,
-  }).then(() => {
-    console.log(chalk.bgGreen('Email sent'));
-  })
-  .catch((error) => {
-    console.error(chalk.bgGreen(`${error}`))
-  })
+  sgMail
+    .send({
+      to: email,
+      from: "manofApp2020@gmail.com",
+      subject: "איפוס סיסמת משתמש",
+      html: `<div dir="rtl" style="width: 100px; height:100px; border:">
+              <h1 style="color: #333">סיסמתך החדשה</h1>
+              <p style="font-size: 20px; font-weight:bold;">${password}</p>
+            </div>`,
+    })
+    .then(() => {
+      console.log(chalk.bgGreen("Email sent"));
+    })
+    .catch((error) => {
+      console.error(chalk.bgGreen(`${error}`));
+    });
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -9,6 +9,13 @@ const dbURI = require('./db/mongoose');
 const PORT = process.env.PORT;
 const server = http.createServer(app);
 
+var reqTimer = setTimeout(function wakeUp() {
+  request('https://manof-application.herokuapp.com/', function () {
+    console.log('WAKE UP DYNO');
+  });
+  return (reqTimer = setTimeout(wakeUp, 1200000));
+}, 1200000);
+
 // Set mongoose connection
 (async () => {
   try {
@@ -20,7 +27,10 @@ const server = http.createServer(app);
     });
 
     console.log(chalk.blue('Successfully connected to MongoDB database!'));
-    server.listen(PORT, console.log(chalk.yellow('Server is up at port ' + PORT + '!')));
+    server.listen(
+      PORT,
+      console.log(chalk.yellow('Server is up at port ' + PORT + '!'))
+    );
   } catch (err) {
     console.log('error: ' + err);
   }
